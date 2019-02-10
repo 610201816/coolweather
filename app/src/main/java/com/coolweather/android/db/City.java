58e18c6ae742ed4094059c1c41849c392499a16e
610201816 <610201816@qq.com>
2019-02-10 14:57:48 +0800

    完成遍历省市县三级列表

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -8,6 +8,14 @@ public class City extends DataSupport {
     private int cityCode;
     private int provinceId;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getCityName() {
         return cityName;
     }
